fac7b60a7da17b8e551916cb5c9f2da12d55dd6d
Shajiah Noor <shajiahnoor621@gmail.com>
2026-07-29 10:39:23 +0500

    HTML, CSS AND JS
    🎨 **Card Making & Form Assignment** 🚀
    
    This project showcases a responsive **Card Design** 🃏 and an interactive **Form** 📝 built using **HTML5**, **CSS3**, and **JavaScript**. It features a clean UI ✨, responsive layout 📱, modern styling 🎨, and JavaScript validation ⚡ to ensure a smooth user experience. This assignment demonstrates my understanding of front-end web development, including responsive design, user interaction, and client-side form validation. 💻🌟

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const form = document.getElementById("profileForm");
+
+const preview = document.getElementById("previewSection");
+
+const cardName = document.getElementById("cardName");
+
+const cardRole = document.getElementById("cardRole");
+
+const cardSkill = document.getElementById("cardSkill");
+
+const downloadBtn = document.getElementById("downloadBtn");
+
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+
+    const name = document.getElementById("fullName").value;
+
+    const role = document.getElementById("role").value;
+
+    const skill = document.getElementById("skill").value;
+
+
+    cardName.textContent = name;
+
+    cardRole.textContent = role;
+
+    cardSkill.textContent = "#" + skill.toUpperCase();
+
+
+    preview.classList.remove("hidden");
+
+});
+
+
+downloadBtn.addEventListener("click", function(){
+
+    window.print();
+
+});
